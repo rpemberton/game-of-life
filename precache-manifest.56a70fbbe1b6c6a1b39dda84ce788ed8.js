6a1e66429ecb87247db1165948ccd91db4a7ff8b
Rob Pemberton <hellorobertp@gmail.com>
2018-12-31 18:42:40 +0000

    Updates

---
+++ precache-manifest.56a70fbbe1b6c6a1b39dda84ce788ed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "26b60bcdfde44a173fd8",
+    "url": "/static/js/main.26b60bcd.chunk.js"
+  },
+  {
+    "revision": "06c9f3604de2271bb9fb",
+    "url": "/static/js/1.06c9f360.chunk.js"
+  },
+  {
+    "revision": "26b60bcdfde44a173fd8",
+    "url": "/static/css/main.56f219aa.chunk.css"
+  },
+  {
+    "revision": "2e105c3b2210b4bdc62200fe81f357a7",
+    "url": "/index.html"
+  }
+];
